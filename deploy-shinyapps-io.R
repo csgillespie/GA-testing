c6ad0bd4680b957c7ec839135f4cc88dc19060ed
Colin Gillespie <csgillespie@gmail.com>
2021-06-15 18:21:26 +0100

    Update

--- deploy-shinyapps-io.R
+++ deploy-shinyapps-io.R
@@ -1,13 +1,15 @@
+## Needs suggests for rsconnect to deploy
 install.packages("rsconnect", dependencies = TRUE)
 if (!requireNamespace("stringr", quietly = TRUE)) install.packages("stringr")
 if (!requireNamespace("cli", quietly = TRUE)) install.packages("cli")
 if (!requireNamespace("remotes", quietly = TRUE)) install.packages("remotes")
 
-message(Sys.getenv('GITHUB_REPOSITORY'))
-message(Sys.getenv("GITHUB_REF"))
-message(Sys.getenv("COMMIT_MESSAGE"))
-message(is.na(Sys.getenv("SHINYAPPS_IO_TOKEN", NA)))
-# Required for packrat to get a repo
+# message(Sys.getenv('GITHUB_REPOSITORY'))
+# message(Sys.getenv("GITHUB_REF"))
+# message(Sys.getenv("COMMIT_MESSAGE"))
+
+# This function would be used if the Shiny app was a package
+# Self install from the branch, then deploy
 # install_pkg = function() {
 #   cli::cli_h1("Install pkg")
 #   path = paste(Sys.getenv('GITHUB_REPOSITORY'), Sys.getenv("GITHUB_REF"), sep = "@")
@@ -16,27 +18,17 @@ message(is.na(Sys.getenv("SHINYAPPS_IO_TOKEN", NA)))
 #   cli::cli_alert_success("{path} installed!")
 # }
 #
+
 deploy = function(account = "jumpingrivers", server = "shinyapps.io") {
   cli::cli_h1("Deploying app")
   rsconnect::setAccountInfo(name = account,
                             token = Sys.getenv("SHINYAPPS_IO_TOKEN"),
                             secret = Sys.getenv("SHINYAPPS_IO_SECRET"))
-  message(Sys.getenv('GITHUB_REPOSITORY'))
-  message(Sys.getenv("GITHUB_REF"))
-  message(Sys.getenv("COMMIT_MESSAGE"))
-  message(is.na(Sys.getenv("SHINYAPPS_IO_TOKEN", NA)))
 
-
-
-
-
-  slug = stringr::str_match(Sys.getenv('GITHUB_REF'), "^refs/heads/(.*)$")
-  message(slug)
-  slug = slug[1, 2]
-  message("slug: ", slug)
-  repo = stringr::str_match(Sys.getenv("GITHUB_REPOSITORY"), ".*/(.*)$")[1, 2]
-  appName = paste(slug, repo, sep = "-")
-  message("appName: ", appName)
+  branch_name = stringr::str_match(Sys.getenv('GITHUB_REF'), "^refs/heads/(.*)$")[1, 2]
+  repo_name = stringr::str_match(Sys.getenv("GITHUB_REPOSITORY"), ".*/(.*)$")[1, 2]
+  appName = paste(repo_name, branch_name sep = "-")
+  cli::cli_alert_info("appName: ", appName)
   rsconnect::deployApp(
     account = account, server = server,
     appDir = ".",
@@ -44,6 +36,7 @@ deploy = function(account = "jumpingrivers", server = "shinyapps.io") {
   cli::cli_alert_success("{appName} successfully deployed")
 }
 
+# Clean up after merging.
 # terminate = function(account = "jumpingrivers", server = "shinyapps.io") {
 #   msg = Sys.getenv("TRAVIS_COMMIT_MESSAGE")
 #   if (stringr::str_detect(msg, "^Merge pull", negate = TRUE)) return(NULL)
